e43aae58c1367dcc5970faa554046dab31b504d4
spencercreer <spencercreer@gmail.com>
2022-05-14 17:04:41 -0700

    textures

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 let camera, scene, renderer, cube;
 
 function init() {
-	// Init scene
 	scene = new THREE.Scene();
-
-	// Init camera (PerspectiveCamera)
 	camera = new THREE.PerspectiveCamera(
 		75,
 		window.innerWidth / window.innerHeight,
@@ -12,41 +9,33 @@ function init() {
 		1000
 	);
 
-	// Init renderer
 	renderer = new THREE.WebGLRenderer({ antialias: true });
-
-	// Set size (whole window)
 	renderer.setSize(window.innerWidth, window.innerHeight);
 
-	// Render to canvas element
 	document.body.appendChild(renderer.domElement);
 
-	// Init BoxGeometry object (rectangular cuboid)
 	const geometry = new THREE.BoxGeometry(3, 3, 3);
 
-	// Create material with color
-	// const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-
-	// Add texture - 
 	const texture = new THREE.TextureLoader().load('textures/headshot.jpg');
-
-	// Create material with texture
 	const material = new THREE.MeshBasicMaterial({ map: texture });
 
-	// Create mesh with geo and material
-	cube = new THREE.Mesh(geometry, material);
-	// Add to scene
+	const materials = [
+		new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture('textures/headshot.jpg')}),
+		new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture('textures/code.jpg')}),
+		new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture('textures/military_satellite.jpg')}),
+		new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture('textures/neuschwanstein_castle.jpg')}),
+		new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture('textures/sweden.jpg')}),
+		new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture('textures/switzerland.jpg')}),
+	]
+
+	cube = new THREE.Mesh(geometry, materials);
 	scene.add(cube);
 
-	// Position camera
 	camera.position.z = 5;
 }
 
-// Draw the scene every time the screen is refreshed
 function animate() {
 	requestAnimationFrame(animate);
-
-	// Rotate cube (Change values to change speed)
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
@@ -54,11 +43,8 @@ function animate() {
 }
 
 function onWindowResize() {
-	// Camera frustum aspect ratio
 	camera.aspect = window.innerWidth / window.innerHeight;
-	// After making changes to aspect
 	camera.updateProjectionMatrix();
-	// Reset size
 	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
