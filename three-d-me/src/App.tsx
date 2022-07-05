04e7acd4ab270429eef795189a51e06a352656bf
spencercreer <spencercreer@gmail.com>
2022-07-05 15:08:00 -0700

    useFrame practice

--- three-d-me/src/App.tsx
+++ three-d-me/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
+import "./styles.css";
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <MyAnimatedBox />
+        <ambientLight args={[0xff0000]} intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} intensity={0.5} />
+      </Canvas>
     </div>
   );
+}
+
+function MyAnimatedBox() {
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime()
+    console.log(`Elapsed time ${a}`);
+  })
+  return (
+    <mesh>
+      <boxGeometry />
+      <meshBasicMaterial color="royalblue" />
+    </mesh>
+  )
 }
 
 export default App;
